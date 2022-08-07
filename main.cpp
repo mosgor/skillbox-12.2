aa9671131a88ed0eef778c3cfcc6aa1aaf00e5f2
Maxim Mikhailov <mos_gor@list.ru>
2022-08-07 10:39:46 +0700

    Implemented heap sort

--- main.cpp
+++ main.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 
-void merge (float *array, int first, int last){
-    int middle = (first + last)/2, start = first, final = middle + 1;
-    float mass[15];
-    for (int i = first; i <= last; ++i) {
-        if((start <= middle && final > last) || array[start] < array[final]){
-            mass[i] = array[start];
-            start++;
-        }
-        else {
-            mass[i] = array[final];
-            final++;
-        }
+void heap (float array[], int size, int root){
+    int largest = root, left = 2 * root + 1, right = 2 * root + 2;
+    if (left < size && array[left] > array[largest]) largest = left;
+    if (right < size && array[right] > array[largest]) largest = right;
+    if (largest != root) {
+        std::swap(array[root], array[largest]);
+        heap(array, size, largest);
     }
-    for (int i = first; i <= last; ++i) array[i] = mass[i];
 }
 
-void merge_sort (float *array, int first, int last){
-    if(first > last){
-        merge_sort(array, first, (first+last)/2);
-        merge_sort(array, (first+last)/2 + 1, last);
-        merge(array, first, last);
+void heap_sort(float array[], int size){
+    for (int i = size / 2 - 1; i >= 0; --i) heap(array, size, i);
+    for (int i = size - 1; i >= 0; --i) {
+        std::swap(array[0], array[i]);
+        heap(array, i, 0);
     }
 }
 
@@ -28,8 +22,8 @@ int main() {
     float unsorted[15];
     std::cout << "Input numbers to sort: ";
     for (int i = 0; i < 15; ++i) std::cin >> unsorted[i];
-    merge_sort(unsorted, 0, 14);
-    for (int i = 0; i < 15; ++i) {
+    heap_sort(unsorted, 15);
+    for (int i = 14; i >= 0; --i) {
         std::cout << unsorted[i] << " ";
     }
     return 0;
